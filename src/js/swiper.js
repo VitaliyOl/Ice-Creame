8eea83bbe8edfb347458d45ae01532e6b9e377d1
Incogn1tto <ageenkopetr48@gmail.com>
2022-12-22 00:59:14 +0100

    update

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,4 +1,8 @@
 var swiper = new Swiper('.mySwiper', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
